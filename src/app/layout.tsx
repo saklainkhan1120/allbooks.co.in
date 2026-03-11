f25aff1fb865d2f0641eabad8417190b4626a797
Saklain Khan <saklainkhan1120@gmail.com>
2026-03-11 10:44:24 +0530

    Add Google Analytics tracking (G-8M879KMYP4) to root layout for traffic tracking on home and product pages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,16 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <meta name="color-scheme" content="light" />
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-8M879KMYP4"></script>
+        <script dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-8M879KMYP4');
+          `
+        }} />
       </head>
       <body
         className={`${poppins.variable} antialiased`}
